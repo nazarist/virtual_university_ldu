f8d484346c5786d3febea80daed1861eb3878d3a
YuraLen <yurii260345@gmail.com>
2023-07-13 15:34:17 +0300

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          РОЗРОБКА БЛЕТ
+          РОЗРОБКА БЛЕТ!
         </p>
         <a
           className="App-link"
