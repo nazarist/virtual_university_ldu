3540675db117c4b76f0f539a5fdff94353a62524
Nazar <109390257+nazarist@users.noreply.github.com>
2023-07-13 15:35:42 +0300

    test vscode2

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,3 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 
+$nazar = 'klwemrfwr';
+$nazar = 'klwemrfwr';
+$nazar = 'klwemrfwr';
+$nazar = 'klwemrfwr';
+$nazar = 'klwemrfwr';
+$nazar = 'klwemrfwr';
+$nazar = 'klwemrfwr';
+$nazar = 'klwemrfwr';
+$nazar = 'klwemrfwr';
