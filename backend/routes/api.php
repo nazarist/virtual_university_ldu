af3dfdac3a8919beb1a7767ff8ec7c37d775ab75
Nazar <109390257+nazarist@users.noreply.github.com>
2023-07-13 15:31:17 +0300

    test vs code

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,16 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/nazar', function (){
-
-    $response = Http::get('http://virt.ldubgd.edu.ua/');
-
-    return $response;
-});
-
-
 
 
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,4 @@
 <?php
 
-
-use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', );
